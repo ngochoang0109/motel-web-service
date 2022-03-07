d7518fc49f363a28f84aa18c34a0e965ae6a6317
hoangtn <hoangtn0109@gmail.com>
2022-03-07 19:21:16 +0700

    create motel be

---
+++ motel-be/src/main/java/com/kltn/motelbe/MotelBeApplication.java
@@ -0,0 +1,13 @@
+package com.kltn.motelbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotelBeApplication.class, args);
+	}
+
+}
---
+++ motel-be/src/test/java/com/kltn/motelbe/MotelBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kltn.motelbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
