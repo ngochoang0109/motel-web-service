7822a3fcea7c64e8ed1142aabf799e04da591b18
hoangtn <hoangtn0109@gmail.com>
2022-03-28 18:44:05 +0700

    update func

--- motel-fe/src/components/user/Layout/PostButton/PostButton.js
+++ motel-fe/src/components/user/Layout/PostButton/PostButton.js
@@ -1,10 +1,28 @@
+import { useAlert } from 'react-alert';
+import { useSelector } from 'react-redux';
 import './PostButton.css';
 
 const PostButton = () => {
+
+    const auth=useSelector(state=>state.authReducer.loggedIn);
+    const alert= useAlert();
+    const goToPostPage=(event)=>{
+        if(auth){
+            console.log("go to post page");
+        }
+        else{
+            alert.show("Đăng nhập để tạo tin rao", {
+                timeout: 3000,
+                type: 'error',
+            });
+            
+        }
+    }
+
     return (
         <div className="create-post-navbar">
             <div className="sub-main">
-                <button className="create-post-btn">Đăng tin</button>
+                <button className="create-post-btn" onClick={goToPostPage}>Đăng tin</button>
             </div>
         </div>
     )
